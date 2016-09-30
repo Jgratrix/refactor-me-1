44cfaede68ba3666352836b34f11a98e9fc3b8b1
James Gratrix <jgratrix@utcmediacityuk.org.uk>
2016-09-30 10:38:02 +0100

    Update RefactorMe.java

--- RefactorMe.java
+++ RefactorMe.java
@@ -6,51 +6,43 @@ public class RefactorMe {
     public static void main(String[] args) {
         getName();
         calculateSalary(2500, 400);
-        calculateSides("square");
+        calculateSides("circle");
         countLetters();
     }
-
+    
     public static void getName(){
-        String name = "Jay";
-        System.out.println(name);
+        System.out.println("Enter Your name");
+        Scanner input = new Scanner(System.in);
+        String word = input.nextLine();
+        System.out.println("Your name is: "+word);
     }
-
+    
     public static void calculateSalary(int income, int tax) {
-        // Salary = income - tax
         int salary = income - tax;
+         System.out.println("Your Salary is: "+salary);
     }
-
+    
     public static void calculateSides(String shape) {
-        // this method should detect for
-        // 1 square
-        // 2 circle
-        // 3 hexagon
-        // 4 dodecagon
-
         if (shape.contains("square")) {
             System.out.println("Your shape has 4 sides");
+        }else  if (shape.contains("circle")) {
+            System.out.println("Your shape has 1 sides");
         }
-        else if (shape.contains("circle")) {
-            System.out.println("Your shape has 1 side");
-        }
-        else if (shape.contains("hexagon")) {
+        else  if (shape.contains("hexagon")) {
             System.out.println("Your shape has 6 sides");
         }
-        else if (shape.contains("dodecagon")) {
+        else  if (shape.contains("dodecagon")) {
             System.out.println("Your shape has 12 sides");
         }
-        else {
-            System.out.println("Please enter a valid shape");
-        }
     }
-
+    
     public static void countLetters() {
+        System.out.println("Enter a word:");
         Scanner input = new Scanner(System.in);
-        String word = input.next();
+        String word = input.nextLine();
         //String word = 
-        System.out.println("You entered: " + word );
-
+        System.out.println("You entered: " + word);
         System.out.println("You word is: " + word.length() + " letters long.");
     }
-
+    
 }
