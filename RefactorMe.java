5b2613e69cc1b57ac4e1034f1362ab186f95c210
James Gratrix <jgratrix@utcmediacityuk.org.uk>
2016-09-30 10:24:51 +0100

    Update RefactorMe.java

--- RefactorMe.java
+++ RefactorMe.java
@@ -4,39 +4,43 @@ import java.util.*;
 public class RefactorMe {
 
     public static void main(String[] args) {
-        getName();
+        getName("Bob");
         calculateSalary(2500, 400);
         calculateSides("circle");
         countLetters();
     }
     
-    public static void getName(){
-        System.out.println("Your age is: ");
+    public static void getName(String name){
+        System.out.println("Your name is: "+name);
     }
     
     public static void calculateSalary(int income, int tax) {
-        // Salary = income - tax
+        int salary = income - tax;
+         System.out.println("Your name is: "+salary);
     }
     
     public static void calculateSides(String shape) {
-        // this method should detect for
-        // 1 square
-        // 2 circle
-        // 3 hexagon
-        // 4 dodecagon
-        
         if (shape.contains("square")) {
             System.out.println("Your shape has 4 sides");
+        }else  if (shape.contains("circle")) {
+            System.out.println("Your shape has 1 sides");
+        }
+        else  if (shape.contains("hexagon")) {
+            System.out.println("Your shape has 6 sides");
+        }
+        else  if (shape.contains("dodecagon")) {
+            System.out.println("Your shape has 12 sides");
         }
     }
     
     public static void countLetters() {
-        
-        
+        System.out.println("Enter a word:");
+        Scanner input = new Scanner(System.in);
+        String word = input.nextLine();
         //String word = 
-        System.out.println("You entered: " + word );
+        System.out.println("You entered: " + word);
         
-        System.out.println("You word is: " + word.length() + "letters long.");
+        System.out.println("You word is: " + word.length() + " letters long.");
     }
     
 }
